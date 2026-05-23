18949f36bee27c8266e9be061e6e35718cdbf104
cnsudstudent1 <cnsudstudent1@gmail.com>
2026-05-23 10:57:15 -0400

    on step 25

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,15 +5,66 @@ using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour
 {
+    //Rigidbody2D object that is stored
+    [Header("Rigidbody")]
+    public Rigidbody2D rb;
+    //downward speed of the object
+    [Header("Default Down Speed")]
+    public float downSpeed = 20f;
+    //movemnet speed of the object
+    [Header("Default Movement Speed")]
+    public float movementSpeed = 10f;
+    //movement direction of the object
+    [Header("Default Directional Movement Speed")]
+    public float movement = 0f;
     // Start is called before the first frame update
     void Start()
     {
-
+        //varible equals to component Rigidbody2D
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // movement equals Horizontal movement
+        //multiplied by the movement speed
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+        //if direction on x axis is less than 0
+        if (movement < 0)
+        {
+            //object faces to the left
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        //if direction on x axis is greater than 0
+        else
+        {
+            //object faces to the right
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
 
+    private void FixedUpdate()
+    {
+        //Vector2 which is (x,y) velocity
+        //equals to the velocity of the rigidbody2D
+        Vector2 velocity = rb.velocity;
+        //Velocity of the x axis equals to
+        //the directtion movement on the x axis
+        // pf the chaacter.
+        velocity.x = movement;
+        //Rigidbody2D velocity equals to
+        //velocity of the object
+        rb.velocity = velocity;
+    }
+
+    //Collision function
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocity = rb.velocity;
+        if (velocity.y <= 0)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, downSpeed, 0);
+        }
     }
 }
